60a7bd49d14e08959b8745879a85be65c39c23ba
Gaurav Raj <gaurav.raj@geeksforgeeks.org>
2024-08-17 23:50:25 +0530

    reactjs jsx

---
+++ myapp2/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+import   Test3  from './Test'
+
+function App() {
+
+  return (
+
+    <div>
+      <h1>Our first Vite App</h1>
+        
+        <Test3 />
+    </div>
+  )
+}
+
+export default App
---
+++ myapp2/src/Test.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Test() {
+  return (
+    <div>
+         <h1>Test Component .....</h1> 
+         <Test2/>
+    </div>
+  )
+}
+
+
+export function Test2(){
+    return (
+        <div>
+              Test Component2 .....
+        </div>
+      )
+
+}
+
+export function Test3(){
+    return (
+        <div>
+              Test Component3 .....
+        </div>
+      )
+
+}
+
+
+
